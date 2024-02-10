9bd45b9ddd521590e351a730c9e1612a24c605e8
adimiuprix <miuprix@gmail.com>
2024-02-10 19:28:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Laravel wizard installer package
+
+![Tests](https://img.shields.io/github/actions/workflow/status/spizian/laravel-wizard-installer/ci.yml?labelColor=444D56&label=Tests)
+![GitHub](https://img.shields.io/github/license/spizian/laravel-wizard-installer?labelColor=444D56&label=License)
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/spizian/laravel-wizard-installer?labelColor=444D56&label=Release)
+
+
+## Installation
+
+```sh
+composer require spizian/laravel-wizard-installer
+```
+
+Code style will be automatically fixed by php-cs-fixer.
---
+++ routes/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+use spizian\wizardtube\Controllers\InstallDatabaseController;
+use spizian\wizardtube\Controllers\InstallFinishController;
+use spizian\wizardtube\Controllers\InstallFolderController;
+use spizian\wizardtube\Controllers\InstallIndexController;
+use spizian\wizardtube\Controllers\InstallKeysController;
+use spizian\wizardtube\Controllers\InstallMigrationsController;
+use spizian\wizardtube\Controllers\InstallServerController;
+use spizian\wizardtube\Controllers\InstallSetDatabaseController;
+use spizian\wizardtube\Controllers\InstallSetKeysController;
+use spizian\wizardtube\Controllers\InstallSetMigrationsController;
+use Illuminate\Support\Facades\Route;
+
+Route::group([
+    'prefix' => 'install',
+    'namespace' => 'spizian\wizardtube\Controllers',
+    'middleware' => ['web', 'installer']
+], static function () {
+    Route::get('/', ['as' => 'install.index', 'uses' => InstallIndexController::class]);
+    Route::get('/server', ['as' => 'install.server', 'uses' => InstallServerController::class]);
+    Route::get('/folders', ['as' => 'install.folders', 'uses' => InstallFolderController::class]);
+    Route::get('/database', ['as' => 'install.database', 'uses' => InstallDatabaseController::class]);
+    Route::post('/database', ['uses' => InstallSetDatabaseController::class]);
+    Route::get('/migrations', ['as' => 'install.migrations', 'uses' => InstallMigrationsController::class]);
+    Route::post('/migrations', ['as' => 'install.migrations', 'uses' => InstallSetMigrationsController::class]);
+    Route::get('/keys', ['as' => 'install.keys', 'uses' => InstallKeysController::class]);
+    Route::post('/keys', ['as' => 'install.keys', 'uses' => InstallSetKeysController::class]);
+    Route::get('/finish', ['as' => 'install.finish', 'uses' => InstallFinishController::class]);
+});
